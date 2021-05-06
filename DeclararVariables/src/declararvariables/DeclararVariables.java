4536099eaa93aad4af0b5e3148470cf74a1bfb53
SayedJPQ <sayedjquiros@gmail.com>
2021-05-06 10:43:31 -0600

    Declarar variables en NetBeans

---
+++ DeclararVariables/src/declararvariables/DeclararVariables.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package declararvariables;
+/**
+ *
+ * @author PC0
+ */
+public class DeclararVariables {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte edad;
+        edad = 18;
+        System.out.println("Tu edad es: " + edad);
+    }
+    
+}
