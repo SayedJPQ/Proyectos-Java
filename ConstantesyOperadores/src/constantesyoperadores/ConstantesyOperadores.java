ce36c07930ad5cda756033d537c657ac2c331a44
SayedJPQ <sayedjquiros@gmail.com>
2021-05-08 11:21:19 -0600

    Constantes y operadores

---
+++ ConstantesyOperadores/src/constantesyoperadores/ConstantesyOperadores.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantesyoperadores;
+/**
+ *
+ * @author PC0
+ */
+public class ConstantesyOperadores {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 5;
+        int b;
+        b = 7;
+        int c=a+b;
+        c++;//Incremento en 1 de c
+        c+=6;//Variante de incremento
+        c-=2;//Decremento variante
+        System.out.println("El resultado de c es: " + c);
+    }
+    
+}
