b821b44aa28ae53189993054a747807b9c2c94b1
SayedJPQ <sayedjquiros@gmail.com>
2021-05-11 10:35:11 -0600

    Cuentas Ahorro incompleta

--- Cuentas/CuentasAhorro.java
+++ Cuentas/CuentasAhorro.java
@@ -1,1 +1,17 @@
 class CuentasAhorro extends Cuentas{}
+private double cuotaMantenimiento;
+public CuentasAhorro(){}
+public CuentasAhorro(String nom, String cue, double cantidadsal,double cantidadre, double tipo, double mant){
+    super(nom,cue,cantidadsal,cantidadre,tipo);
+    asignarCuotaMante(mant);
+}
+public void asignarCuotaMante(double cantidad) {
+    if (cantidad < 0){
+        System.out.println("Error: Cantidad de saldo negativa");
+        return;
+    }
+    cuotaMantenimiento = cantidad;
+}
+public double obtenerCuentaMante(){
+    return cuotaMantenimiento;
+}
--- Cuentas/Test.java
+++ Cuentas/Test.java
@@ -2,23 +2,23 @@ public class Test
 {
   public static void main(String[] args)
   {
-    Cuentas cuenta01 = new Cuentas();
+    CuentasAhorro cuenta01 = new CuentasAhorro();
     cuenta01.asignarNombre("Sayed");
     cuenta01.asignarCuenta("01");
     cuenta01.asignarTipoDeInteres(2.5);
     cuenta01.ingreso(40000);
     cuenta01.reintegro(12000);
-    Cuentas cuenta02 = new Cuentas("Josue","02",70000,20000,3.5);
+    //CuentasAhorro cuenta02 = new CuentasAhorro("Josue","02",70000,20000,3.5);
     System.out.println();
     System.out.println(cuenta01.obtenerNombre());
     System.out.println(cuenta01.obtenerCuenta());
     System.out.println(cuenta01.estado());
     System.out.println(cuenta01.obtenerTipoDeInteres());
-    System.out.println();
-    System.out.println(cuenta02.obtenerNombre());
-    System.out.println(cuenta02.obtenerCuenta());
-    System.out.println(cuenta02.estado());
-    System.out.println(cuenta02.obtenerTipoDeInteres());
+    //System.out.println();
+    //System.out.println(cuenta02.obtenerNombre());
+    //System.out.println(cuenta02.obtenerCuenta());
+    //System.out.println(cuenta02.estado());
+    //System.out.println(cuenta02.obtenerTipoDeInteres());
     //Println deja un salto de linea y print no
   }
 }
